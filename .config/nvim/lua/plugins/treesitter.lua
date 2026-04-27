0aa1f7ee7fa809d8c77a5cf34c30787fb53902da
Alan Cheng <coolbluealan@gmail.com>
2026-04-26 23:32:23 -0400

    nvim: treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,38 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "bash",
-        "c",
-        "c_sharp",
-        "cpp",
-        "css",
-        "fish",
-        "gdscript",
-        "html",
-        "htmldjango",
-        "java",
-        "javascript",
-        "lua",
-        "markdown",
-        "python",
-        "rust",
-        "sql",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
-        "zathurarc",
-      },
+    require("nvim-treesitter").install({
+      "bash",
+      "c",
+      "c_sharp",
+      "cpp",
+      "css",
+      "fish",
+      "gdscript",
+      "html",
+      "htmldjango",
+      "java",
+      "javascript",
+      "lua",
+      "markdown",
+      "python",
+      "rust",
+      "sql",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+      "zathurarc",
+    })
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-
-      indent = {
-        enable = true,
-      },
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "*" },
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
     })
   end,
 }
